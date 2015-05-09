e2ccc2c447c71566fb8e37ac3c93f7c4e845ef76
WaPonX <18826419224@163.com>
2015-05-09 21:55:22 +0800

    Signed-off-by: WaPonX <18826419224@163.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
-from win import mainwindows
-
-mainwindows()
+import sys
+from win import windows
+from conductfile import CreateDirectory
+from PyQt5 import QtWidgets
+ 
+data = CreateDirectory()
+app = QtWidgets.QApplication(sys.argv)    
+win = windows()
+win.SetWindow(len(data[0]), data[2])
+win.show()
+win.ReflushAxis(data, button_width = 50, spacing = 10)
+sys.exit(app.exec_())
